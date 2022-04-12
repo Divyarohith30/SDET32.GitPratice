32535fb480c16b7d8f8464399ba26af5ac64369f
rohithgowda <rohithgowda@DivyaRohithGowda>
2022-04-12 05:00:05 -0400

    commit5

---
+++ com.crm.comcast.SDET32GitPracticse/src/test/java/com/crm/comcast/SDET32GitPracticse/ClassD.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32GitPracticse;
+
+import org.testng.annotations.Test;
+
+public class ClassD {
+	@Test
+	public void demoDTest() {
+		System.out.println("login into class-D");
+		System.out.println("VERIFY ");
+		System.out.println("logout from class-D");
+
+	}
+}

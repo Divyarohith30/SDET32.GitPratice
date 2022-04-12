ca13735d8422ac3f46e9b8c266020b37d5ed3d40
rohithgowda <rohithgowda@DivyaRohithGowda>
2022-04-12 03:19:31 -0400

    commit2

---
+++ com.crm.comcast.SDET32GitPracticse/src/test/java/com/crm/comcast/SDET32GitPracticse/ClassB.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32GitPracticse;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	@Test
+	public void demoTest() {
+		System.out.println("------Modification start----- -");
+		System.out.println("Second Push of ClassB");
+		System.out.println("------Modification end----- -");
+
+	}
+}
---
+++ com.crm.comcast.SDET32GitPracticse/src/test/java/com/crm/comcast/SDET32GitPracticse/ClassC.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.SDET32GitPracticse;
+
+import org.testng.annotations.Test;
+
+public class ClassC {
+	@Test
+	public void demoTest() {
+		System.out.println("------SECOND COMMIT start----- -");
+		System.out.println("classC Push ");
+		System.out.println("------SECOND COMMIT end----- -");
+
+	}
+}

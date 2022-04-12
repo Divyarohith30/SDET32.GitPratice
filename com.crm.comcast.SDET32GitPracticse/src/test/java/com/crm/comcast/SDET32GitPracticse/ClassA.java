94219936a678e9c582b7f5d428a3ecf4f403dd2a
rohithgowda <rohithgowda@DivyaRohithGowda>
2022-04-12 02:07:56 -0400

    commit1

---
+++ com.crm.comcast.SDET32GitPracticse/src/test/java/com/crm/comcast/SDET32GitPracticse/ClassA.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32GitPracticse;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+@Test
+public void demoTest() {
+	System.out.println("First Push");
+	
+}
+}

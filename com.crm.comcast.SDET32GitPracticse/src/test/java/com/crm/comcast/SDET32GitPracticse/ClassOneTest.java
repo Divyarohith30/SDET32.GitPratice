157584f961ce723044792ddaa6f628031ab2bfcf
rohithgowda <rohithgowda@DivyaRohithGowda>
2022-04-14 00:59:20 -0400

    j1

---
+++ com.crm.comcast.SDET32GitPracticse/src/test/java/com/crm/comcast/SDET32GitPracticse/ClassOneTest.java
@@ -0,0 +1,16 @@
+package com.crm.comcast.SDET32GitPracticse;
+
+import org.testng.annotations.Test;
+
+public class ClassOneTest {
+	@Test
+	public void jenkisTest() {
+		String browser=System.getProperty("browser");
+		String url=System.getProperty("url");
+
+
+		System.out.println("First from jenkins");
+		System.out.println("second from jenkins");
+		System.out.println("third from jenkins");
+	}
+}
